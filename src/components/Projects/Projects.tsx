ffa2baf8595c2d0f7cc0aba1c70021cbf9299c8c
Filipe Junqueiro <filipe.jubqueiro.2005@gmail.com>
2025-09-22 18:18:59 +0100

    refactor(ProjectSlides): Moved Slide Data for its own File

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,8 +1,9 @@
 import { Carousel } from "@mantine/carousel";
-import { Button, Paper, Text, Title, useMantineTheme } from "@mantine/core";
+import { Button, Paper, Title, useMantineTheme } from "@mantine/core";
 import { useMediaQuery } from "@mantine/hooks";
 import classes from "./Projects.module.css";
 import type { ProjectCardProps } from "../../types/ProjectCardProps";
+import { ProjectSlides } from "./ProjectSlides";
 
 function openWin(link: string) {
   window.open(link, "_blank");
@@ -36,37 +37,10 @@ function Card({ image, title, link }: ProjectCardProps) {
   );
 }
 
-const data = [
-  {
-    image:
-      "https://files.gta5mod.net/mods/2025/09/Benefactor-Panto-ForFour-480x360.png",
-    title:
-      "A simple and modular vehicle system built for FiveM (W.I.P | Repo sill private)",
-    link: "https://github.com/PFScripts/pf_vehicles",
-  },
-  {
-    image:
-      "https://storage.googleapis.com/medium-feed.appspot.com/images%2F9353691196%2Fd584cef5994fe-O-que-e-Svelte-e-por-que-voce-deve-considera-lo.jpg",
-    title: "A simple boilerplate to build svelte apps for FiveM",
-    link: "https://github.com/PFScripts/fivem_lua_svelte_boilerplate",
-  },
-  {
-    image:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAKfo4KCYDLhxEmXOngH0KYfOyApAf59lzKQ&s",
-    title: "A simple boilerplate to build react apps for FiveM",
-    link: "https://github.com/PFScripts/fivem_react_vite_mantine_boilerplate",
-  },
-  {
-    image: "https://cdn.hswstatic.com/gif/water-update.jpg",
-    title: "A simple water refill system built for FiveM",
-    link: "https://github.com/PFScripts/pf_wrs",
-  },
-];
-
 function Projects() {
   const theme = useMantineTheme();
   const mobile = useMediaQuery(`(max-width: ${theme.breakpoints.sm})`);
-  const slides = data.map((item) => (
+  const slides = ProjectSlides.map((item) => (
     <Carousel.Slide key={item.title}>
       <Card {...item} />
     </Carousel.Slide>
